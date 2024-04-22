2408fcc6562505e08e2ca9b88624a31d40f73c05
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2024-04-22 13:15:53 -0400

    Update install_ros2_humble.sh

--- install_ros2_humble.sh
+++ install_ros2_humble.sh
@@ -17,7 +17,7 @@ main() {
   RUN=$(user_can_sudo && echo "sudo" || echo "command")
   $RUN apt update && $RUN apt install curl -y
   $RUN curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | $RUN tee /etc/apt/sources.list.d/ros2.list > /dev/null
   
   
   $RUN apt update -y
