86f59ea3591683950a713cfd35d0a534bcd68bf0
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2025-03-13 16:44:24 -0400

    Update install_lunar_vim.sh

--- install_lunar_vim.sh
+++ install_lunar_vim.sh
@@ -17,9 +17,11 @@ SUDO="" # To support docker environment
 fi
 
 # To install Lunar Vim, Neo-Vim == 0.9.0 is required (in Ubuntu 22.04)
-wget https://github.com/neovim/neovim/releases/download/v0.9.0/nvim.appimage
-$SUDO chmod u+x nvim.appimage && ./nvim.appimage --appimage-extract
-
+# And to use VimTex, it required > 0.10.0
+wget https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
+tar -xzvf nvim-linux64.tar.gz
+$SUDO mv nvim-linux64 /usr/local/
+$SUDO ln -s /usr/local/nvim-linux64/bin/nvim /usr/bin/nvim
 # Install pip
 $SUDO apt-get install python3-pip -y
 
@@ -31,12 +33,21 @@ $SUDO apt install build-essential ca-certificates curl gnupg -y
 $SUDO apt-get install -y nodejs
 $SUDO apt-get install -y npm
 
+$SUDO npm install -g n
+$SUDO n lts
+$SUDO n latest
+$SUDO n prune
+$SUDO npm install tree-sitter-cli
+
 # ripgrep
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
 $SUDO dpkg -i ripgrep_13.0.0_amd64.deb
 
 # Install Lunar Vim
-LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
+
+# For clipboard synchronization support
+$SUDO apt-get install xclip xsel
 
 # Nerd Font for kitti icons
 # Once you run `install_vim_and_vundle.sh`, below commands are not needed
