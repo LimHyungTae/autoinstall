c98a82aaa468d6c7d174607bd53e6c2ff2b70e76
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2025-03-24 11:11:55 -0400

    Update and rename install_ros2_humble.sh to install_ros2_jazzy.sh

--- install_ros2_jazzy.sh
+++ install_ros2_jazzy.sh
@@ -19,10 +19,9 @@ main() {
   $RUN curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | $RUN tee /etc/apt/sources.list.d/ros2.list > /dev/null
   
-  
-  $RUN apt update -y
+  $RUN apt update -y && $RUN apt install ros-dev-tools
   $RUN apt upgrade -y
-  $RUN apt install ros-humble-desktop -y
+  $RUN apt install ros-jazzy-desktop -y
 
   $RUN apt install python3-colcon-common-extensions
   if [ "$*" == "bash" ]; then
