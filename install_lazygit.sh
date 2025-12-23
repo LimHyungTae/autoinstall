502300aa0d1dbbfc4e1027e22618f6a85189e363
Hyungtae Lim <shapelim@mit.edu>
2025-12-22 22:45:04 -0500

    Update lazygit

---
+++ install_lazygit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+
+user_can_sudo() {
+  command_exists sudo || return 1
+  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+}
+
+main() {
+  RUN=$(user_can_sudo && echo "sudo" || echo "command")
+  
+  $RUN apt-get update
+
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  echo "Downloading lazygit..."
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  $RUN install lazygit /usr/local/bin
+  }
+
+main 
